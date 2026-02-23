c1b4942a0af6cae371a4331de0a174dd59084d7f
titokawa <titokawa.biz@gmail.com>
2026-02-23 10:47:41 +0900

    CRUD complete with Go and MySQL

---
+++ main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"html/template"
+	"net/http"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+type PageData struct {
+	Message string
+	Tasks []Task
+}
+
+type Task struct {
+	ID int
+	Content string
+}
+
+
+func main() {
+
+	var err error
+
+	db, err = sql.Open("mysql", "root:9999@unix(/tmp/mysql.sock)/mermaid")
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("DB接続成功")
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/delete", deleteHandler)
+	http.HandleFunc("/edit", editHandler)
+	http.HandleFunc("/update", updateHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+    var name string
+
+	if r.Method == "POST"{
+		name := r.FormValue("name")
+
+		_, err := db.Exec("INSERT INTO tasks (content) VALUES (?)", name)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	rows, err := db.Query ("SELECT id, content FROM tasks")
+	if err != nil {
+		panic(err)
+	}
+    defer rows.Close()
+
+	var tasks []Task
+
+    for rows.Next(){
+	var t Task
+	err := rows.Scan(&t.ID, &t.Content)
+	if err != nil {
+		panic(err)
+	}
+		tasks = append(tasks, t)
+}	
+
+		data := PageData{
+			Message: name,
+			Tasks: tasks,
+		}
+	
+		tmpl.Execute(w, data)
+	}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request){
+	if r.Method != "POST"{
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	id := r.FormValue("id")
+
+	_, err := db.Exec("DELETE FROM tasks WHERE id = ?", id)
+	if err != nil {
+		panic(err)
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func editHandler(w http.ResponseWriter, r * http.Request){
+	id := r.URL.Query().Get("id")
+
+	row := db.QueryRow("SELECT id, content FROM tasks WHERE id = ?", id)
+
+	var task Task
+	err := row.Scan(&task.ID, &task.Content)
+	if err != nil {
+		panic(err)
+	}
+	tmpl, _ := template.ParseFiles("templates/edit.html")
+	tmpl.Execute(w, task)
+}
+
+func updateHandler(w http.ResponseWriter, r *http.Request){
+	if r.Method != "POST"{
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	id := r.FormValue("id")
+	content := r.FormValue("content")
+
+	_, err := db.Exec("UPDATE tasks SET content = ? WHERE id = ?", content, id)
+	if err != nil {
+		panic(err)
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
